808bdac6e6f5e60eea67bd125946f806c6b47b98
AndreasAAR <Andreas.ahrlund@gmail.com>
2020-05-04 22:02:12 +0200

    "Bash test first files"

---
+++ scriptTest.sh
@@ -0,0 +1,6 @@
+  GNU nano 4.9                                                  test.sh                                                   Modified
+#!/bin/sh 
+#Basic training script, just seeing if cygwin can run shell properly.
+echo "Pickans Model" FavLPHBooks.txt
+cp -v favLPHBooks.txt testFolder 1> copyLog.txt
+
---
+++ variableTypeTest.sh
@@ -0,0 +1,14 @@
+!##!/usr/bin/env bash
+#Testing types
+number=16
+string="String"
+command="echo "hello""
+
+echo $string
+echo $number
+$command
+
+#outputs
+#String
+#16
+#hello
